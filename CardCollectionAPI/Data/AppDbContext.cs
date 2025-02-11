8bccae048c7c97d71c6aff50b5e67729559f499c
Ortu21 <ortmat21@gmail.com>
2025-02-11 21:36:37 +0100

    add DbSet properties for PokemonSet, PokemonAttack, PokemonWeakness, PokemonResistance, and PokemonPrice in AppDbContext

--- CardCollectionAPI/Data/AppDbContext.cs
+++ CardCollectionAPI/Data/AppDbContext.cs
@@ -1,10 +1,23 @@
-using Microsoft.EntityFrameworkCore;
 using CardCollectionAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CardCollectionAPI.Data
 {
     public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
     {
         public DbSet<PokemonCard> PokemonCards { get; set; }
+        public DbSet<PokemonSet> PokemonSets { get; set; }
+        public DbSet<PokemonAttack> PokemonAttacks { get; set; }
+        public DbSet<PokemonWeakness> PokemonWeaknesses { get; set; }
+        public DbSet<PokemonResistance> PokemonResistances { get; set; }
+        public DbSet<PokemonPrice> PokemonPrices { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PokemonPrice>()
+                .HasOne(p => p.PokemonCard)
+                .WithOne(c => c.Price)
+                .HasForeignKey<PokemonPrice>(p => p.PokemonCardId);
+        }
     }
 }
