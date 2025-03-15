6d0615df79360ea35dd0b3a0915612f87cf7d786
Ortu21 <ortmat21@gmail.com>
2025-03-15 19:55:13 +0100

    update 15 files

--- CardCollectionAPI/obj/Debug/net9.0/CardCollectionAPI.AssemblyInfo.cs
+++ CardCollectionAPI/obj/Debug/net9.0/CardCollectionAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CardCollectionAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f631f3d3c6e6a07beaa719d4c3852864f146244")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+245b5954641ea1e69120694a940e856d0dad8e31")]
 [assembly: System.Reflection.AssemblyProductAttribute("CardCollectionAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CardCollectionAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
