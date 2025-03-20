cfe1b6d4d2c4cb2629fe66d920fa57d5b0a6643d
Ortu21 <ortmat21@gmail.com>
2025-03-20 20:04:34 +0100

    assembly

--- CardCollectionAPI/obj/Debug/net9.0/CardCollectionAPI.AssemblyInfo.cs
+++ CardCollectionAPI/obj/Debug/net9.0/CardCollectionAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CardCollectionAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea8c02611d3d781783b4beb5df8476fd116f3b60")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0720d9d32efb2cadfddefd9457e86425b797f6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CardCollectionAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CardCollectionAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
